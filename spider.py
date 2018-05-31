2ba6b2f60a15ace788e0abf0c4c631d578672dec
yurunyang1998 <674860268@qq.com>
2018-05-31 09:44:57 +0800

    打包成了函数，获得了字典

--- spider.py
+++ spider.py
@@ -4,27 +4,52 @@ user = '05166115'
 pwd = '166115'
 
 
-url_get_session = 'https://api.xiyoumobile.com/xiyoulibv2/user/login/?username='+user+'&password='+pwd+'&callback=jsonp5'
-#data = {'user_name':user,'user_pwd':pwd}
-r  = requests.get(url_get_session)
-true =1
-false =0
-null =0
-r = r.text
-#print(r)
-r = r.split('(')[1].split(')')[0]
+def getsession(user,pwd):
+    url_get_session = 'https://api.xiyoumobile.com/xiyoulibv2/user/login/?username='+user+'&password='+pwd+'&callback=jsonp5'
+    #data = {'user_name':user,'user_pwd':pwd}
+    r  = requests.get(url_get_session)
+    true =1
+    false =0
+    null =0
+    r = r.text
+    #print(r)
+    r = r.split('(')[1].split(')')[0]
 
-#print(r)
-session_dic = dict(eval(r))
+    #print(r)
+    session_dic = dict(eval(r))
 
-#print(session_dic['Detail'])
-session = session_dic['Detail'].split()[0]
+    #print(session_dic['Detail'])
+    session = session_dic['Detail'].split()[0]
+
+    return session    #获取到了session
 
 
 
-url_get_book_information = 'https://api.xiyoumobile.com/xiyoulibv2/user/rent/?session='+session+'%20Path=/opac_two'
-r = requests.get(url_get_book_information)
-r = dict(eval(r.text))
-print(r['Detail'])
-for i in r['Detail']:
-    print(i['Title'])
+def getbookinfo(session):
+    true = 1
+    false = 0
+    null = 0
+    url_get_book_information = 'https://api.xiyoumobile.com/xiyoulibv2/user/rent/?session='+session+'%20Path=/opac_two'
+    r = requests.get(url_get_book_information)
+    r = dict(eval(r.text))
+
+
+    for i in r['Detail']:
+        try:
+            print(i.keys())
+            bookinfo={'Title':i['Title'],
+                      'state':i['State'],
+                      'Date':i['Date'],
+                      'Library_id':i['Library_id'],
+                      'canrenew':i['CanRenew'],
+                      'department_id':i['Department_id'],
+                      'department':i['Department']}
+            print(bookinfo)
+            
+        except Exception as e:
+            print(e)
+            #exit(0)
+
+if __name__ == '__main__':
+    session = getsession(user,pwd)
+    getbookinfo(session)

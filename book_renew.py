678895b4b0d6254f5e922cff326437c626d87858
yurunyang1998 <674860268@qq.com>
2018-06-05 22:22:04 +0800

    靠

--- book_renew.py
+++ book_renew.py
@@ -1,12 +1,20 @@
 from flask import Flask
-
+from flask import  request
 app = Flask(__name__,static_url_path='')
 
 
-@app.route('/',methods=['GET','POST'])
-def hello_world():
-    return app.send_static_file(filename='html/sign.html')
+@app.route('/',methods=['GET'])
+def index():
+    return app.send_static_file(filename='yuRunYang/html/sign.html')
 
+@app.route('/book_renew',methods=['GET','POST'])
+def get_message():
+    user = request.form(['name'])
+    pwd = request.form(['password'])
+    email = request.form(['email'])
+
+    print(user)
+    return user
 
 if __name__ == '__main__':
     app.run()
